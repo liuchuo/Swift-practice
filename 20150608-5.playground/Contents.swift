cd76c6bb83a743f96d0eafe08d18c260498a5910
liuchuo <95323362@qq.com>
2015-06-08 16:10:10 +0800

    基本运算符1

---
+++ 20150608-5.playground/Contents.swift
@@ -0,0 +1,80 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var intResult = 1 + 2
+println(intResult)
+
+intResult = intResult - 1
+println(intResult)
+
+intResult = intResult * 2
+println(intResult)
+
+intResult = intResult / 2
+println(intResult)
+
+intResult = intResult + 8
+intResult = intResult % 7
+println(intResult)
+
+println("-----------")
+var doubleResult = 10.0
+println(doubleResult)
+
+doubleResult = doubleResult - 1
+println(doubleResult)
+
+doubleResult = doubleResult * 2
+println(doubleResult)
+
+doubleResult = doubleResult / 2
+println(doubleResult)
+
+doubleResult = doubleResult + 8
+doubleResult = doubleResult % 7
+println(doubleResult)
+
+var s1tr = "Hello, playground"
+println(s1tr)
+
+var value1 = 1
+var value2 = 2
+if value1 == value2{
+    println("value1 == value2")
+}
+else{
+    println("value != value2")
+}
+
+let name1 = "world"
+let name2 = "world"
+if name1 == name2{
+    println("name1 == name2")
+}
+let a1 = [1,2]
+let a2 = [1,2]
+if a1 == a2 {
+    println("a1 == a2")
+}
+
+var i = 0
+var a = 10
+var b = 9
+if(a > b) || (i++ == 1){
+    println("或运算为true")
+} else {
+    println("或运算为false")
+}
+println("i = \(i)")
+
+i++
+println("i = \(i)")
+
+
+
+
+
+
+
