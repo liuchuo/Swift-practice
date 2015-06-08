5ecd0cf93efb872aa8cd2a595cb8fa148df077f0
liuchuo <95323362@qq.com>
2015-06-08 17:02:43 +0800

    基本数据类型1

---
+++ 20150608-7.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+println("UInt8 range:\(UInt8.min)~\(UInt8.max)")
+println("Int8 range:\(Int8.min)~\(Int8.max)")
+println("UInt range:\(UInt.min)~\(UInt.max)")
+println("UInt64 range:\(UInt64.min)~\(UInt64.max)")
+println("Int range:\(Int.min)~\(Int.max)")
+
+var ageForStudent = 30
+ageForStudent = 20
+
+
+var test1:Int = 30
+
+var myMoney:Float = 300.5
+var yourMoney:Double = 400.9
+
+let pi = 3.1415926
+
+var mymy = 3.36e2
+var interstRate = 1.56e-2
+
+var hehe = 000.0156
+var haha = 3_360_000
+
+let historyScore:UInt8 = 90
+let englishScore:UInt16 = 130
+let totalScore = UInt16(historyScore) + englishScore
+let total1 = historyScore + UInt8(englishScore)
+let b = 3
+println("\(b)")
+println("\(total1)")
