f7a8fbdb1874912490e9fab82940dd30549a272b
liuchuo <95323362@qq.com>
2015-06-08 13:03:28 +0800

    hello world

---
+++ 20150608-1/20150608-1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  20150608-1
+//
+//  Created by 欣 陈 on 15/6/8.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+import UIKit
+
+var str = "Hello World"
+println(str)
---
+++ 20150608-2.playground/Contents.swift
@@ -0,0 +1,5 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+let π = 3.14159
+let _Hello = "Hello"
