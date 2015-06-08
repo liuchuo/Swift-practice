b9d26b230e6da32c6e4f0e8949e72147439cb696
liuchuo <95323362@qq.com>
2015-06-08 13:22:33 +0800

    20150608-5

---
+++ 20150608-4.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var a1 = 10
+var a2 = 20
+var a = a1 > a2 ? "a1" : "a2"
+
+var str = "Hello, playground"
+var b1:Int = 10
+let b2:Int = 20         //指定数据类型
+var b = b1 > b2 ? "b1" : "b2"
+var c1:Int = 10;var c2:Int = 20;
+var m = 10
+var n = -m
+
+
