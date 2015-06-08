6b4cfb746595664dda77358d1f291e59f51eaddc
liuchuo <95323362@qq.com>
2015-06-08 16:30:49 +0800

    基本运算符2

---
+++ 20150608-6.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let a: UInt8 = 0b10110010
+let b: UInt8 = 0b01011110
+
+println("a | b = \(a | b)")
+println("a & b = \(a & b)")
+println("a ^ b = \(a ^ b)")
+println("~a = \(~a)")
+
+println("a >> 2 = \(a >> 2)")
+println("a << 2 = \(a << 2)")
+
+let c: Int8 = -0b1100
+
+println("c >> 2 = \(c >> 2)")
+println("c << 2 = \(c << 2)")
+
+var airports = ["TYO":"Tokyo","DUB":"Dublin"]
+println(airports)
+
