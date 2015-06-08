2ba9a8279edc0a55152a76a0fc6994cab34a5c5f
liuchuo <95323362@qq.com>
2015-06-08 17:35:10 +0800

    卖个萌= =喵~

---
+++ 20150608-8.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var 🐒 = true
+var 🐼 : Bool = false
+var 小欣欣 :Bool = false
+var 陈恺垣 = true
+if(陈恺垣 == 🐒){
+    println("咩哈哈哈~~~")
+}
+else{
+    println("啦啦啦啦~~")
+}
+if(小欣欣 == 🐼){
+    println("喵喵喵~~~")
+}
