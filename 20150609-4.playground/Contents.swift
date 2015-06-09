b2a6360ce7f0b652004c8f250f4de1980505041d
liuchuo <95323362@qq.com>
2015-06-09 18:32:51 +0800

    字符串比较

---
+++ 20150609-4.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//字符串比较
+let 熊: String = "🐻"
+let 猫: String = "🐱"
+if(熊 > 猫){
+    println("🐻 > 🐱")
+} else {
+    println("🐻 < 🐱")
+}
