ae2a5ec63648aa8b608b19e9ebcec2601b69f6ff
liuchuo <95323362@qq.com>
2015-06-09 15:32:51 +0800

    字符和字符串1

---
+++ 20150609-1.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let andSign1:Character = "&"
+let andSign2 = "\u{26}"
+let lamda1:Character = "λ"
+let lamda2 = "\u{03bb}"
+let smile1 : Character = "😀"
+let smile3 = "\u{001f602}"
+let smile4 = "\u{001f602}"
+let smile5 = "\u{001f602}"
+let smile6 = "\u{001f602}"
+let smile7 = "\u{001f701}"
+let lalala = "(●'◡'●)ﾉ♥"
+let hahaha = "ヾ(*´∀｀*)ﾉ "
+
