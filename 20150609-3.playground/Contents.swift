29e781ab69d1b9dc77ec7c2bfd39e0831bab258a
liuchuo <95323362@qq.com>
2015-06-09 17:15:20 +0800

    诺亚方舟乘客数~

---
+++ 20150609-3.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var 🌎 = "🐶🐺🐱🐭🐹🐰🐸🐯🐨🐻🐷🐦"
+
+🌎 = 🌎 + "🌺"
+🌎 += "🍀"
+println("诺亚方舟乘客数:\(count(🌎))")
+
+let number = 9
+let total = "\(number) 加 10 等于 \(Double(number) + 10)"
+
+
