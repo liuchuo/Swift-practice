a361afdca8888edddb84cd4fd27754acbdb1591e
liuchuo <95323362@qq.com>
2015-06-13 22:33:32 +0800

    函数类型

---
+++ 20150613-1.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+func rectangleArea(width : Double , height : Double) -> Double {
+    let area = width * height
+    return area
+}
+
+func triangleArea(bottom : Double , height : Double) -> Double {
+    let area = 0.5 * bottom * height
+    return area
+}
+
+func sayhello() {
+    println("Hello,world~~~")
+}
+
