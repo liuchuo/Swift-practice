d6b1f41306a993ae31030c7df4afe993b53b46a3
liuchuo <95323362@qq.com>
2015-06-19 23:16:12 +0800

    iOS框架he和库模板

---
+++ 20150619-7/20150619-7/20150619-7.h
@@ -0,0 +1,19 @@
+//
+//  20150619-7.h
+//  20150619-7
+//
+//  Created by 欣 陈 on 15/6/19.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for 20150619-7.
+FOUNDATION_EXPORT double _0150619_7VersionNumber;
+
+//! Project version string for 20150619-7.
+FOUNDATION_EXPORT const unsigned char _0150619_7VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <_0150619_7/PublicHeader.h>
+
+
