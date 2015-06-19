1acff8222b918cd6173c623fe4e0ef3ca3e10e20
liuchuo <95323362@qq.com>
2015-06-19 23:14:07 +0800

    ios应用程序工程模板

---
+++ 20150619-6/20150619-6Tests/_0150619_6Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _0150619_6Tests.swift
+//  20150619-6Tests
+//
+//  Created by 欣 陈 on 15/6/19.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _0150619_6Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
