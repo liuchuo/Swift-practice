d704787199e7c3d89d2b9768d58feaf8de39542e
liuchuo <95323362@qq.com>
2015-07-25 23:24:34 +0800

    总是装逼失败真的好嘛~~

--- 20150725-2.playground/Contents.swift
+++ 20150725-2.playground/Contents.swift
@@ -2,4 +2,32 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class Employee {
+    var no : Int = 0
+    var firstname : String = "liu"
+    var lastname : String = "chuo"
+    var job : String?
+    var salary : Double = 0
+    lazy var dept : Department = Department()
+    
+    var fullName : String {
+        get {
+            return firstname + "." + lastname
+        }
+        set (newFullName) {
+            var name = newFullName.componentsSeparatedByString(".")
+            firstname = name[0]
+            lastname = name[1]
+        }
+    }
+}
+    
+    struct Department {
+        let no : Int = 0
+        var name : String = ""
+    }
+    var emp = Employee()
+    println(emp.fullName)
+    
+    emp.fullName = "chen.xin"
+    println(emp.fullName)
