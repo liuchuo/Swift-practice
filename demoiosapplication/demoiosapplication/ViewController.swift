0d7c62ddc121029e02e28629d500ddada4305f58
liuchuo <95323362@qq.com>
2015-09-10 22:57:11 +0800

    demoiosapplication

---
+++ demoiosapplication/demoiosapplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoiosapplication
+//
+//  Created by 欣 陈 on 15/9/10.
+//  Copyright (c) 2015年 欣 陈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
