5305ae847a9ae2fa866b79c3293999d2e96cf2ba
liuchuo <95323362@qq.com>
2017-05-05 21:00:22 +0800

    -

---
+++ mySwift3.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// 简单输出
+print("hello, world")
+
+
+// let声明常量，var声明变量
+let myConstant = 11
+
+var myVariable1 = 42
+myVariable1 = 56
+
+
+// 可以让编译器推断常量或者变量的类型，也可以自己显式指定
+var myVariable2:Double = 6
+
+
+// 值永远的不会被显式转换为其他类型，必须显式转换
+let string1 = "the label is"
+let integer1 = 94
+let string2 = string1 + String(integer1)
+
+
+// 
+
